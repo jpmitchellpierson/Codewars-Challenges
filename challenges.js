50ebfcc0f39c44e5da416a7b1c50d1e2efce670a
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-28 14:12:08 -0700

    deep count

--- challenges.js
+++ challenges.js
@@ -1,4 +1,33 @@
-// *** String Average Level 6 *** //
+// Array.length will give you the number of top-level elements in an array.
+// Your task is to create a function deepCount that 
+// returns the number of ALL elements within an array, 
+// including any within inner-level arrays.
+
+// For example:
+// deepCount([1, 2, 3]);  --> 3
+// deepCount(["x", "y", ["z"]]);  --> 4
+// deepCount([1, 2, [3, 4, [5]]]); --> 7 
+
+function deepCount(a){
+  let total = 0;
+  
+  function countElements(array) {
+    total += array.length;
+    console.log(total)
+    
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].constructor === Array) {
+        countElements(array[i]);
+      } 
+    }
+  }
+  
+  countElements(a);
+  return total;
+}
+
+
+// *** String Average 6 KYU *** //
 // You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
 
 // "zero nine five two" -> "four"
@@ -22,3 +51,4 @@ function averageString(str) {
   avg = Math.floor(sum/arr.length);
   return numToStr[avg];
 }
+
