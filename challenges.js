720b97e3871c2d60f8648a363e91a07cb2e26c62
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-29 12:40:19 -0700

    rotate array

--- challenges.js
+++ challenges.js
@@ -1,4 +1,4 @@
-// *** Deep Count 6 KYU *** //
+// *** DEEP COUNT 6 KYU *** //
 // Array.length will give you the number of top-level elements in an array.
 // Your task is to create a function deepCount that 
 // returns the number of ALL elements within an array, 
@@ -28,7 +28,7 @@ function deepCount(a){
 }
 
 
-// *** String Average 6 KYU *** //
+// *** STRING AVERAGE 6 KYU *** //
 // You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
 
 // "zero nine five two" -> "four"
@@ -53,3 +53,47 @@ function averageString(str) {
   return numToStr[avg];
 }
 
+// *** ROTATE ARRAY 6 KYU *** //
+// Create a function named "rotate" that takes an array and 
+// returns a new one with the elements inside rotated n spaces.
+
+// If n is greater than 0 it should rotate the array to the right. 
+// If n is less than 0 it should rotate the array to the left. 
+// If n is 0, then it should return the array unchanged.
+
+// Example:
+// var data = [1, 2, 3, 4, 5];
+
+// rotate(data, 1) // => [5, 1, 2, 3, 4]
+// rotate(data, 2) // => [4, 5, 1, 2, 3]
+// rotate(data, 3) // => [3, 4, 5, 1, 2]
+// rotate(data, 4) // => [2, 3, 4, 5, 1]
+// rotate(data, 5) // => [1, 2, 3, 4, 5]
+
+// rotate(data, 0) // => [1, 2, 3, 4, 5]
+
+// rotate(data, -1) // => [2, 3, 4, 5, 1]
+// rotate(data, -2) // => [3, 4, 5, 1, 2]
+// rotate(data, -3) // => [4, 5, 1, 2, 3]
+// rotate(data, -4) // => [5, 1, 2, 3, 4]
+// rotate(data, -5) // => [1, 2, 3, 4, 5]
+function rotate(array,n){
+  let arr = []
+  let temp;
+  let i = 0;
+  if (n === 0) return array;
+  
+  array.map(e => arr.push(e));
+  
+  for (let i = 0; i < n; i++) {
+    temp = arr.pop();
+    arr.unshift(temp);
+  }
+  
+  for (let j = 0; j > n; j--) {
+    temp = arr.shift();
+    arr.push(temp);
+  }
+  
+  return arr;
+}
