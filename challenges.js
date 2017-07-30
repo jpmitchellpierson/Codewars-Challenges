3985e9e930853b19146df5104d09c538d54446cd
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-30 15:56:38 -0700

    isPrime

--- challenges.js
+++ challenges.js
@@ -97,3 +97,18 @@ function rotate(array,n){
   
   return arr;
 }
+
+
+// *** IS A PRIME NUMBER? 6 KYU *** //
+// Define a function isPrime that takes one integer argument and 
+// returns true or false depending on if the integer is a prime.
+// Per Wikipedia, a prime number (or a prime) is 
+// a natural number greater than 1 that has no positive divisors 
+// other than 1 and itself.
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
