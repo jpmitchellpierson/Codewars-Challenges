4226c59a9a55db11741165c27ebf6d391f38b6b3
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-07 13:04:33 -0700

    find the unique number

--- challenges.js
+++ challenges.js
@@ -181,6 +181,31 @@ function duplicateCount(text){
 }
 
 
+// *** FIND THE UNIQUE NUMBER 6 KYU *** //
+// There is an array with some numbers 
+// All numbers are equal except for one
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains more than 3 numbers.
+function findUniq(arr) {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!obj[arr[i]]) {
+      obj[arr[i]] = 1;
+    } else {
+      obj[arr[i]]++;
+    }
+  }
+
+  let keys = Object.keys(obj);
+  for (let k = 0; k < keys.length; k++) {
+    if (obj[keys[k]] === 1) {
+      return Number(keys[k]);
+    }
+  }
+}
+
+
 // *** Band Name Generator 7 KYU *** //
 // My friend wants a new band name for her band. 
 // She like bands that use the formula: 'The' + a noun with first letter capitalized.
