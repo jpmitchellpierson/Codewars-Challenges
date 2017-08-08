2a927d9e86ccd6e66cf28020ede0ae9f478a7a6a
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-08 16:46:32 -0700

    odd or even

--- challenges.js
+++ challenges.js
@@ -224,3 +224,14 @@ function bandNameGenerator(str) {
   let s = str.charAt(0).toUpperCase() + str.slice(1);
   return str[0] === str[str.length - 1] ? s + str.slice(1) : 'The ' + s;
 }
+
+// *** Odd Or Even 7 KYU *** //
+// Given an array of numbers, determine whether the sum of all of the numbers is odd or even.
+// Give your answer in string format as 'odd' or 'even'.
+// If the input array is empty consider it as: [0] (array with a zero).
+// oddOrEven([0]) returns "even"
+// oddOrEven([2, 5, 34, 6]) returns "odd"
+// oddOrEven([0, -1, -5]) returns "even"
+function oddOrEven(array) {
+  return array.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+}
