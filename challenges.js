381d9c77c94ede0b53aac20be75c2d186634f820
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-09 14:17:43 -0700

    multiples of 3 and 5

--- challenges.js
+++ challenges.js
@@ -181,7 +181,25 @@ function duplicateCount(text){
 }
 
 
-// *** FIND THE UNIQUE NUMBER 6 KYU *** //
+// *** Multiples Of Three And Five 6 KYU *** //
+// If we list all the natural numbers below 10 
+// that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+// The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all 
+// the multiples of 3 or 5 below the number passed in.
+// If the number is a multiple of both 3 and 5, only count it once.
+function solution(num){
+  let i = 0, sum = 0;
+  while (i < num) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+    i++;
+  }
+  return sum;
+}
+
+// *** Find The Unique Number 6 KYU *** //
 // There is an array with some numbers 
 // All numbers are equal except for one
 // findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
