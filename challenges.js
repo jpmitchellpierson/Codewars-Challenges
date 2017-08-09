2acfab1f398af09da3b0a74af826f51a98085b07
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-09 12:52:14 -0700

    fold your way to the moon

--- challenges.js
+++ challenges.js
@@ -235,3 +235,20 @@ function bandNameGenerator(str) {
 function oddOrEven(array) {
   return array.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
 }
+
+// *** Fold Your Way To The Moon 7 KYU *** //
+// You know that a piece of paper has a thickness of 0.0001m 
+// Given distance in units of meters, 
+// calculate how many times you have to fold the paper 
+// to make the paper reach this distance.
+function foldTo(distance) {
+  let thickness = 0.0001;
+  let folds = 0;
+  
+  if (distance < 0 || distance === 0) return null;
+  while (thickness < distance) {
+    thickness *= 2;
+    folds++;
+  }
+  return folds;
+}
