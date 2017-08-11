38e862776b1bac12ca24b9fff7e9f36e60b6e8e3
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-10 17:31:26 -0700

    reverse a number

--- challenges.js
+++ challenges.js
@@ -270,3 +270,19 @@ function foldTo(distance) {
   }
   return folds;
 }
+
+// *** Reverse A Number 7 KYU *** //
+// Given a number, write a function to output its reverse digits. 
+// (e.g. given 123 the answer is 321)
+// Numbers should preserve their sign 
+// i.e. a negative number should still be negative when reversed.
+function reverseNumber(n) {
+  let res, slice;
+  if (n < 0) {
+    slice = n.toString().slice(1);
+    res = -Number(slice.split('').reverse().join(''));
+  } else {
+    res = Number(n.toString().split('').reverse().join(''));
+  }
+  return res;
+}
