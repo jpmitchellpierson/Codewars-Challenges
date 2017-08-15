8b3d39ffa8cecd30083194e0859b9c37a68c3e58
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-15 13:11:56 -0700

    square digits

--- challenges.js
+++ challenges.js
@@ -180,8 +180,21 @@ function averageString(str) {
   return numToStr[avg];
 }
 
-
-
+// *** Square Digits *** //
+// Welcome. In this kata, you are asked to square every digit of a number.
+// For example, if we run 9119 through the function, 811181 will come out.
+// Note: The function accepts an integer and returns an integer
+function squareDigits(num){
+  let res, i;
+  let sqrdArr = [];
+  let a = num.toString().split('');
+  
+  for (i = 0; i < a.length; i++) {
+    let sqrd = (a[i]*a[i]).toString();
+    sqrdArr.push(sqrd);
+  }
+  return Number(sqrdArr.join(''))
+}
 
 // *** Is A Prime Number? *** //
 // Define a function isPrime that takes one integer argument and 
