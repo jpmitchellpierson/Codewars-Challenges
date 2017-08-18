77b05e8da151ea98dcf1a4c27f6bd08986d59e93
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-17 17:39:00 -0700

    a function within a function

---
+++ 8 KYU/function-within-function.js
@@ -0,0 +1,6 @@
+// Given an input n, write a function always that returns a function which returns n. 
+// var three = always(3);
+// three(); // returns 3
+function always (n) {
+  return () => n;
+}
