f93ae7c1abea3cd8a37e59def34ca63c366fc4b4
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-24 17:45:22 -0700

    fix

--- 8 KYU/basic-variable-assignment.js
+++ 8 KYU/basic-variable-assignment.js
@@ -1,3 +1,4 @@
+// *** Basic Variable Assignment *** //
 // This code should store "codewa.rs" as a variable called "name" but it's not working. Can you figure out why?
 var a = "code";
 var b = "wa.rs";
