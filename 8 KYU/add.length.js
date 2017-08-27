ff4f66f8df0258e0723b63dbe03921cc190a0be3
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-27 16:07:20 -0700

    add length

---
+++ 8 KYU/add.length.js
@@ -0,0 +1,11 @@
+// *** Add Length *** //
+// Write a function that takes a String and returns an Array/list with the length of each word added to each element
+// addLength('apple ban') => ["apple 5", "ban 3"]
+// addLength('you will win') => ["you 3", "will 4", "win 3"]
+function addLength(str){
+  let arr = str.split(' ');
+  return arr.reduce((a, c) => { 
+    a.push(c + ' ' + c.length); 
+    return a; 
+  }, []);
+}
