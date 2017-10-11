83610735e4fb373f1371ec6723ca588faf6dd904
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-10-11 15:38:23 -0700

    palindrome chain length

---
+++ 5 KYU/palindrome-chain-length.js
@@ -0,0 +1,28 @@
+// *** Palindrome Chain Length *** //
+// Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes and 43, 194, 4773 are not palindromes.
+
+// Write a method palindrome_chain_length which takes a positive number and returns the number of special steps needed to obtain a palindrome. The special step is: "reverse the digits, and add to the original number". If the resulting number is not a palindrome, repeat the procedure with the sum until the resulting number is a palindrome.
+
+// If the input number is already a palindrome, the number of steps is 0.
+// Input will always be a positive integer.
+// For example, start with 87:
+// 87 + 78 = 165; 165 + 561 = 726; 726 + 627 = 1353; 1353 + 3531 = 4884
+function palindromeChainLength(n) {
+  if (checkForPalindrome(n)) return 0;
+  
+  let res = 0;
+  while (!checkForPalindrome(n)) {
+    n = sumReversed(n);
+    res++;
+    checkForPalindrome(n);
+  }
+  return res;
+}
+
+function checkForPalindrome(num) {
+  return num === Number(num.toString().split('').reverse().join('')) ? true : false;
+}
+
+function sumReversed(num) {
+  return num + Number(num.toString().split('').reverse().join(''));
+}
