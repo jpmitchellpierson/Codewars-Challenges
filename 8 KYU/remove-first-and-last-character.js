f40716f097a07d69879a98b4a20f59e75c77c637
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-10-14 14:49:50 -0700

    remove first and last character

---
+++ 8 KYU/remove-first-and-last-character.js
@@ -0,0 +1,5 @@
+// *** Remove First And Last Character *** //
+// Create a function that removes the first and last characters of a string
+function removeChar(str){
+  return str.substr(1, str.length - 2); 
+}
